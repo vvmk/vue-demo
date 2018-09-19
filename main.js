decb36eee5761da72680a95f331930a4df24fff4
vvmk <vincentmasiello@gmail.com>
2018-09-19 00:15:29 -0400

    Apply the 'selected' style when selecting a tab

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 Vue.component('tabs', {
-    template: `<div><div class="tabs">
-  <ul>
-    <li v-for="tab in tabs">
-        <a href="#">{{ tab.name }}</a>
-    <li>
-  </ul>
-</div>
+    template: `
+<div>
+    <div class="tabs">
+        <ul>
+            <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                <a href="#" @click="selectTab(tab)">{{ tab.name }}</a>
+            </li>
+        </ul>
+    </div>
 
     <div class="tabs-details">
-    <slot></slot>
-    <div></div>`,
+        <slot></slot>
+    </div>
+</div>`,
     mounted() {
         console.log(this.$children);
     },
@@ -21,13 +24,29 @@ Vue.component('tabs', {
     created() {
         this.tabs = this.$children;
     },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        },
+    },
 });
 
 Vue.component('tab', {
     props: {
         name: { required: true },
+        selected: { default: false },
     },
     template: `<div><slot></slot></div>`,
+    data() {
+        return {
+            isActive: false,
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    },
 });
 
 Vue.component('modal', {
