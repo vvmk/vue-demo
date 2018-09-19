c7341a9fbcac852c443595ee1cf2c0bfa8e13466
vvmk <vincentmasiello@gmail.com>
2018-09-18 23:59:44 -0400

    Set up tabs, tab components. Grab the name prop.

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+Vue.component('tabs', {
+    template: `<div><div class="tabs">
+  <ul>
+    <li class="is-active"><a>Pictures</a></li>
+    <li><a>Music</a></li>
+    <li><a>Videos</a></li>
+    <li><a>Documents</a></li>
+  </ul>
+</div>
+    
+    <div class="tabs-details">
+    <slot></slot>
+    <div></div>`,
+    mounted() {
+        console.log(this.$children);
+    },
+});
+
+Vue.component('tab', {
+    props: {
+        name: { required: true },
+    },
+    template: `<div><slot></slot></div>`,
+});
+
 Vue.component('modal', {
     template: `<div class="modal is-active">
                 <div class="modal-background"></div>
