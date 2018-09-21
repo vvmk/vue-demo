745cb9bb63e890900196d4c3f41e3df1795f9f93
vvmk <vincentmasiello@gmail.com>
2018-09-20 23:50:50 -0400

    Add a new modal with some named slots

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// window.Event = new Vue();
 window.Event = new class {
     constructor() {
         this.vue = new Vue();
@@ -85,16 +84,25 @@ Vue.component('tab', {
 
 Vue.component('modal', {
     template: `<div class="modal is-active">
-                <div class="modal-background"></div>
-                <div class="modal-content">
-                    <div class="box">
-                        <p>
-                        <slot></slot>
-                        </p>
-                    </div>
-                </div>
-                <button class="modal-close is-large" aria-label="close" @click="$emit('closed')"></button>
-            </div>`
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+
+        <slot name="header"></slot>
+
+      </p>
+      <button class="delete" aria-label="close" @click="$emit('closed')"></button>
+    </header>
+    <section class="modal-card-body">
+        <slot name="content"></slot>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>`
 });
 
 Vue.component('custom-display', {
