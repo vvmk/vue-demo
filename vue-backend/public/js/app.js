871b812d3437314c72c180348347c4b87cb485c5
vvmk <vincentmasiello@gmail.com>
2018-09-22 17:45:21 -0400

    Create a Laravel app to demonstrate Vue integration
    add axios (3rd party ajax library with Promises out of the box)

---
+++ vue-backend/public/js/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#root',
+
+    mounted() {
+        // Make an ajax request to our server. (/skills endpoint)
+        // options: fetch() (need a polyfill)
+        // $.ajax(), $.getJson() ...nah
+        // axios <-- popular with vue people
+    },
+});
+
---
+++ vue-backend/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Remember: returning something directly from this method 
+// (something that isn't a view) it is automatically converted
+// into a JSON object. Neat!
+Route::get('skills', function () {
+    return [ 'Laravel', 'Vue', 'PHP', 'JavaScript', 'Trollin\'' ];
+});
