6cef28d3faa1638516d4c71d2e896367032aced9
vvmk <vincentmasiello@gmail.com>
2018-09-22 18:17:52 -0400

    Make an ajax request to /skills and render the response array

--- vue-backend/public/js/app.js
+++ vue-backend/public/js/app.js
@@ -1,11 +1,12 @@
 new Vue({
     el: '#root',
 
+    data: {
+        skills: []
+    },
+
     mounted() {
-        // Make an ajax request to our server. (/skills endpoint)
-        // options: fetch() (need a polyfill)
-        // $.ajax(), $.getJson() ...nah
-        // axios <-- popular with vue people
+        axios.get('/skills').then(response => this.skills = response.data);
     },
 });
 
--- vue-backend/resources/views/welcome.blade.php
+++ vue-backend/resources/views/welcome.blade.php
@@ -65,8 +65,12 @@
     </head>
     <body>
         <div id="root">
-        
+            <ul>
+                <li v-for="skill in skills" v-text="skill"></li>
+            </ul>
         </div>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
         <script src="/js/app.js"></script>
     </body>
