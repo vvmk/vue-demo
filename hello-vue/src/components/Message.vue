ddf3e86d3f2b25d29485d59f8aa817fa134f8fc6
vvmk <vincentmasiello@gmail.com>
2018-09-22 14:51:13 -0400

    Create a component a vue component module and render it in app

---
+++ hello-vue/src/components/Message.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: "hey there"
+    };
+  }
+};
+</script>
+
+<style>
+.box {
+  background: gray;
+  border: 1px solid black;
+  margin-bottom: 20px;
+}
+</style>
